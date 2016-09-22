72083d4209eeefa1b92d13501a876b44aea787d2
Tameika Lawrence <tameikalawrence@Tameikas-MacBook-Air.local>
2016-09-22 18:04:40 -0400

    Done...Fin...Byeeee: Trivia

--- swift-fibo-finder-lab/ViewController.swift
+++ swift-fibo-finder-lab/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func solveTrivia(statesAndCapitals: [String:String]) -> String {
+        
+        //let state = ""
+        //let capital = ""
+        
+        
+        for (state, capital) in statesAndCapitals {
+            
+            let stateChars = state.characters.map{String($0) }
+            print(stateChars)
+            
+            let capitalChars = capital.characters.map{String($0) }
+            print(capitalChars)
+            
+            for _ in stateChars {
+                
+                if stateChars != capitalChars {
+                    
+                    let found = stateChars.joined(separator: "")
+                    
+                    
+                    return found
+                    
+                    
+                } else {
+                    
+                    
+                }
+                
+            }
+        }
+        
+        return "no answer found"
+    }
+    
 }
+
+
 
